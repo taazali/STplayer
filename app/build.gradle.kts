1c8e0ac61232ca413b28aef4ae87a20ea93c2432
taazali <74545990+taazali@users.noreply.github.com>
2025-06-29 04:10:39 +0400

    Upgrade to AGP 8.2.2 and SDK 34 for modern dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.taazali.stplayer"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.taazali.stplayer"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
